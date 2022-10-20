bc1d5183e263c6c7680aa5ab5bfa722a0c96b441
Hélio Nunes <helioaugustofn@protonmail.com>
2022-10-20 10:59:15 -0400

    Adicionado operadores de atribuição

--- operators.js
+++ operators.js
@@ -1,35 +1,20 @@
-/* 
-    let x = a + b;
-    let x = (100 + 50 ) * a;
-*/
-
+// Operadores aritiméticos
 let x = 5;
 let y = 2;
-let z = x + y;
-console.log(z);
+let z = x + y; // 7
+z = x - y; // 3
+z = x * y; // multiplicação
+z = x / y; // 2.5
+z = x % y; // operador módulo (resto da divisão). Resultado: 1
+z = x ** 2; // operador de exponenciação. Resultado: 25
+// esse código produz o mesmo resultado da operação anterior
+z = Math.pow(x, 2); // 25 x^2
 
-z = x - y;
-console.log(z);
-
-z = x * y;
-console.log(z);
-
-z = x / y;
-console.log(z);
-
-z = x % y;
-console.log(z);
-
-z = x;
-z++;
-console.log(z);
-
-z--;
-console.log(z);
-
-z = x ** 2;
-console.log(z);
-
-// esse código produz o mesmo resultado do anterior
-z = Math.pow(x, 2);
-console.log(z);
+// Operadores de atribuição
+z = x; // 5
+z++; // incremento + 1 -> equivalente a z = z + 1 ou z += 1. Resultado: 6
+z--; // decremento - 1 -> equivalente a z = z - 1 ou z -= 1. Resultado: 4
+z += 1; // equivalente a z++. Resultado: 5
+z -= 1; // equivalente a z--. Resultado: 4
+z *= 2; // equivalente a z = z * 2. Resultado: 8
+z /= 2; // equivalente a z = z / 2. Resultado: 4
