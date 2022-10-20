e7c8706fbaf5ba990e9f3a9e5d0e395c3b8bb241
Hélio Nunes <helioaugustofn@protonmail.com>
2022-10-20 09:51:59 -0400

    Notas sobre tipos em JavaScript

---
+++ dataTypes.js
@@ -0,0 +1,21 @@
+// Boolean
+// o tipo booleano só tem dois valores: true ou false (pode-se pensar como sim ou não)
+let nameFieldChecked = true;
+let ageFieldChecked = false;
+
+// valores booleanos também aparecem como resultado de comparações
+let isGreater = 4 > 1; // true
+
+/*
+    O valor null
+    o valor especial null não pertence a nenhum dos tipos descritos anteriormente
+    No JavaScript, null não é uma referência a um objeto não-existente ou um ponteiro nulo como
+    em algumas outras linguagens.
+    É um valor especial que representa o nada, vazio ou valor desconhecido.
+*/
+let age = null;
+
+/*
+    O valor undefined
+    
+*/
